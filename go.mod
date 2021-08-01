51d5f7a2148b182684396262d488d5e5a056516a
simenghe <simengschool@gmail.com>
2021-07-31 23:16:01 -0400

    Init Commit

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/simenghe/urlshort
+
+go 1.16
+
+require (
+	github.com/go-chi/chi v1.5.4
+	github.com/go-chi/chi/v5 v5.0.3
+	github.com/go-chi/httprate v0.5.1 // indirect
+	github.com/google/uuid v1.3.0
+)
---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"fmt"
+	"net/http"
+	"os"
+	"time"
+
+	"github.com/go-chi/chi/middleware"
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/httprate"
+	"github.com/google/uuid"
+)
+
+/*
+Create an http server
+GET request will generate a key using uuid?
+Each uuid key will map to URL.
+Another request will get resolved url and redirect
+user to the site via html
+*/
+
+// cache finds us the string from the query
+var (
+	cache map[string]string
+	PORT  = os.Getenv("PORT")
+)
+
+func generateKey() string {
+	return uuid.New().String()[0:8]
+}
+
+func handleGenerateLink(w http.ResponseWriter, r *http.Request) {
+	fmt.Println(r.URL)
+	URI := r.URL.Query().Get("url")
+	key := generateKey()
+
+	cache[key] = URI
+	fmt.Fprintf(w, "%v mapped to %v", URI, key)
+}
+func handleGetLink(w http.ResponseWriter, r *http.Request) {
+	key := chi.URLParam(r, "key")
+	URI, ok := cache[key]
+	fmt.Println(URI, ok, key)
+	if !ok {
+		http.Error(w, "key does not exist", http.StatusInternalServerError)
+		return
+	}
+	http.Redirect(w, r, URI, http.StatusSeeOther)
+}
+
+func main() {
+	cache = make(map[string]string)
+	r := chi.NewRouter()
+
+	r.Use(middleware.RequestID)
+	r.Use(middleware.RealIP)
+	r.Use(middleware.Logger)
+	r.Use(middleware.Recoverer)
+	r.Use(httprate.LimitByIP(50, 1*time.Minute))
+
+	r.Get("/api/generatelink", handleGenerateLink)
+	r.Get("/api/getlink/{key}", handleGetLink)
+	http.ListenAndServe(":8080", r)
+}
